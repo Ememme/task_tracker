bf60816685933b8e9d434a42e4659f4dff68264b
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-08-29 10:37:50 +0200

    Add fetch API

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,30 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require_tree .
-$(function(){ $(document).foundation(); });
-
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require rails-ujs
 //= require foundation
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require_self
 
 $(function(){ $(document).foundation(); });
 
@@ -42,12 +25,18 @@ $(function(){
     var input = $(event.target)
     var query = input.val()
 
-    $.ajax({
-      method: "GET",
-      url: "/tasks/search",
-      data: { query: query }
+    // input = "/tasks/search?query="+query
+    input = new Request("/tasks/search?query="+query);
+
+    init = {
+      method: 'GET'
+    }
+
+    fetch(input)
+    .then(function(response) {
+      return response.json();
     })
-    .done(function( suggestions ) {
+    .then(function(suggestions) {
       var $suggestions = $('#suggestions')
       var $suggestions_table = $suggestions.find('table tbody')
 
@@ -59,6 +48,28 @@ $(function(){
 
       $suggestions.show()
     });
+
+    // ==================================================
+    // ================  jQuery version =================
+    // ==================================================
+
+    // $.ajax({
+    //   method: "GET",
+    //   url: "/tasks/search",
+    //   data: { query: query }
+    // })
+    // .done(function( suggestions ) {
+    //   var $suggestions = $('#suggestions')
+    //   var $suggestions_table = $suggestions.find('table tbody')
+
+    //   $suggestions_table.empty()
+
+    //   suggestions.forEach(function(suggestion) {
+    //     $suggestions_table.append('<tr><td>' + suggestion + '</td></tr>')
+    //   });
+
+    //   $suggestions.show()
+    // });
   })
 
   $('#suggestions table').on('click', function(event){
